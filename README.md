3b3510a1ea0149581c037058aa3a726417452404
alexandraruth <66081217+alexandraruth@users.noreply.github.com>
2021-06-22 16:10:58 -0400

    updates to code and README

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 + This is code for pulling tweets from Maryland County Health Department Twitter pages to analyze tweets about COVID-19 vaccination strategies.
 
+
+
+# General observations
+
++ Some counties do not have twitter accounts but use Facebook or instagram instead as their primary social media accounts for disseminating information
+
+
+
+# Elements that make Twitter communication about vaccination more helpful and accessible
+
++ Including other languages (e.g. information about vaccine clinics in Spanish)
++ Information about clinics that are: walk-in, no ID required, 
++ Maximizing textual information and minimizing image-based information so that vision-impaired users can access information using text readers
++ Information specific to certain groups: Seniors, people with other underlying conditions, people with disabilities, etc. 
++ Phone numbers to call for assistance
++ Information on mobile clinics
++ Presence of positive incentives for vaccination
+
+# Current next steps
+
++ Search, by county, for information about positive incentives for vaccination & document steps and search terms
--- mddoh_counties_tweets.R
+++ mddoh_counties_tweets.R
@@ -44,13 +44,13 @@ mddoh_handles <- mddoh_handles[!is.na(mddoh_handles)]
 
 mddoh_handles <- sub('@', '', mddoh_handles)
 
-#### Get tweets from MD county health departments ----
+#### Get tweets from MD county health departments & state ----
 
 # input academic twitter API bearer token
 
 bearer_token <- "<token>"
 
-#### Query twitter API with md doh twitter handles
+#### Query twitter API with MD DOH county & state twitter handles
 
 mddoh_tweets <- get_user_tweets(mddoh_handles,  # users object 
                               start_tweets = "2021-01-01T00:00:00Z", # start date
@@ -60,17 +60,15 @@ mddoh_tweets <- get_user_tweets(mddoh_handles,  # users object
 )
 
 
-mddoh_tweets <- get_user_tweets("@pgchealth",  # users object 
-                                start_tweets = "2021-01-01T00:00:00Z", # start date
-                                end_tweets = "2021-06-22T00:00:00Z", # end date
-                                bearer_token = bearer_token, # bearer token 
-                                file = "mddoh_tweets"
-)
-
-
 #list out first 20 tweets
+head(mddoh_tweets$text, 20)
+
+# subset and create a new data frame with only tweets about xyz
+mddoh_tweets_walkin <- mddoh_tweets %>%
+  filter(grepl("walk-in", text))
 
 
+#### to re-load data:
 
+mddoh_tweets <- readRDS("mddoh_tweets.rds")
 
-#### 
